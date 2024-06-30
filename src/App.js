08c36430231e0ed8258524ece4b9708dde0c4c97
moisgames <VLAD981010@gmail.com>
2024-06-30 10:52:34 +0700

    Two-way binding of components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React, { useState } from 'react';
 
 function App() {
   const [likes, setLikes] = useState (999)
-
-  function increment () {
-    setLikes (likes + 1)
-  }
-
-  function decrement () {
-    setLikes (likes - 1)
-
-  }
-
-
+  const [value, setValue] = useState ('Текст в инпуте')
   return (
     <>
        <h1>Total Likes : {likes}</h1>
+       <h1>{value}</h1>
+       <input type='text'
+              value={value}
+              onChange={event => setValue(event.target.value)}>
+                
+              </input>
        <button onClick={increment}>Increment</button>
        <button onClick={decrement}>Dicrement</button>
     </>
