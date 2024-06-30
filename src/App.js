11ece481af1e234a03fe2f298430fd003c221a4b
moisgames <VLAD981010@gmail.com>
2024-06-30 20:56:27 +0700

    Learning Props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
-import Counter from './components/Counter';
-import Twocat from './components/TwoCat';
-import Bear from './components/Bear';
-import ClassCounter from './components/ClassCounter';
-
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import Twocat from "./components/TwoCat";
+import Bear from "./components/Bear";
+import ClassCounter from "./components/ClassCounter";
+import "./styles/app.css";
+import PostItem from "./components/PostItem";
 
 function App() {
   return (
-    <div className='App'>
-      <ClassCounter/>
-  </div>)
+    <div className="App">
+    <PostItem post = {{id: 1, title: 'Javascript', body: 'Description'}}/>
+    <PostItem post = {{id: 2, title: 'Javascript', body: 'Description'}}/>
+    <PostItem post = {{id: 3, title: 'Javascript', body: 'Description'}}/>
+    <PostItem post = {{id: 4, title: 'Javascript', body: 'Description'}}/>
+    <PostItem post = {{id: 5, title: 'Javascript', body: 'Description'}}/>
+    </div>
+  );
 }
 
 export default App;
